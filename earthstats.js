3d1118d1265559de5518999e6ef6556c592bcba4
Cameron Tremblay <catremblay33@gmail.com>
2025-08-16 02:07:54 -0400

    add js for north america section

---
+++ earthstats.js
@@ -0,0 +1,9 @@
+const northAmericaArea = document.getElementById('north-america');
+
+northAmericaArea.addEventListener('click', () => {
+  const cards = document.querySelectorAll('.stats-card');
+  cards.forEach(card => card.classList.remove('active'));
+
+  const naCard = document.getElementById('north-america-card');
+  naCard.classList.add('active');
+});
